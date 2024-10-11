5b1d840a7407eda0018201744091f9e33938663f
nan <ipfilus@gmail.com>
2024-10-11 00:27:29 -0300

    feat: add task model and task create/get-all routes

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+tasks = []
+task_id_control = 1
 
-@app.route('/about')
-def about():
-    return 'About!'
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control += 1
+    tasks.append(new_task)
+    return jsonify({'message': 'Task created successfully'}), 201
+    
+@app.route('/tasks/all', methods=['GET'])
+def get_all_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    total_tasks = len(tasks)
+    return jsonify({
+        'tasks': task_list,
+        'total_tasks': total_tasks 
+    }), 200
 
 if __name__ == '__main__':
     app.run(debug=True) 
